56620e9662d1e0259ae88b60c5d8d71098f6de22
o3m2a1r <103195436+o3m2a1r@users.noreply.github.com>
2026-01-18 19:48:27 +0300

    Create a repository for storing responses

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -15,7 +16,13 @@ namespace PartyInvites.Controllers
         {
             return View();
         }
+        [HttpGet]
         public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
         {
             return View();
         }
